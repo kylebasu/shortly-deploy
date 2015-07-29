f353c6a6ca7886f693af65f7dc985c6a5c550cb9
kylebasu <kylebasu@gmail.com>
2015-07-29 11:56:29 -0700

    rawr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
-// var port = 4568;
+var port = process.env.PORT || 4568;
 
-app.listen(process.env.PORT});
+app.listen(port);
 
-console.log('Server now listening on port ' + process.env.PORT);
+console.log('Server now listening on port ' + port);
